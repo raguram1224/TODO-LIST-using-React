17180ec3e21aac3f3f052a0dcc6525c7000a742a
raguram1224 <raguramvk18@gmail.com>
2023-01-09 14:03:39 +0530

    add TODO-LIST-using-React master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import AddTaskForm from './components/AddTaskForm.jsx';
+import UpdateForm from './components/UpdateForm.jsx';
+import ToDo from './components/ToDo.jsx';
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
+
+  //Tasks ToDo List(Tasks)
+  const [toDo, setToDo] = useState([]);
+
+  //Temp State
+  const [newTask, setNewTask] = useState(' ');
+  const [UpdateData, setUpdateData] = useState('');
+
+  //Add Task
+
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false }
+      setToDo([...toDo, newEntry])
+      setNewTask(' ');
+    }
+  }
+
+
+  //Delete Task
+
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter(task => task.id !== id)
+    setToDo(newTasks);
+  }
+
+  //marktask as done or completed
+
+  const markDone = (id) => {
+    let newTask = toDo.map(task => {
+      if (task.id === id) {
+        return ({ ...task, status: !task.status })
+      }
+      return task;
+    })
+    setToDo(newTask)
+  }
+
+  //cancelupdate
+
+  const cancelUpdate = () => {
+    setUpdateData(' ');
+  }
+
+  //change task for update
+
+  const changeTask = (e) => {
+    let newEntry = {
+      id: UpdateData.id,
+      title: e.target.value,
+      status: UpdateData.status ? true : false
+    }
+    setUpdateData(newEntry);
+  }
+
+  //update task
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter(task => task.id !== UpdateData.id);
+    let updatedObject = [...filterRecords, UpdateData]
+    setToDo(updatedObject);
+    setUpdateData(' ');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container App">
+      <br /> <br />
+
+      <h2>Build TODO LIST using REACT JS</h2>
+      <br /> <br />
+
+
+      {/*Update Task */}
+      {UpdateData && UpdateData ? (
+        <UpdateForm
+
+          UpdateData={UpdateData}
+          updateTask={updateTask}
+          cancelUpdate={cancelUpdate}
+          changeTask={changeTask}
+        />
+      ) : (
+
+        <AddTaskForm
+          newTask={newTask}
+          setNewTask={setNewTask}
+          addTask={addTask}
+
+        />
+      )}
+
+
+
+      {/* Display ToDos */}
+      {toDo && toDo.length ? ' ' : 'No Tasks...'}
+
+      <ToDo
+        toDo={toDo}
+        markDone={markDone}
+        setUpdateData={setUpdateData}
+        deleteTask={deleteTask}
+      />
+    </div >
   );
 }
 
---
+++ src/components/UpdateForm.jsx
@@ -0,0 +1,24 @@
+const UpdateForm = ({ UpdateData, updateTask, cancelUpdate, changeTask }) => {
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <input value={UpdateData && UpdateData.title}
+                        onChange={(e) => changeTask(e)}
+                        className="form-control form-control-lg" />
+
+
+                </div>
+                <div className="col-auto">
+                    <button onClick={updateTask}
+                        className="btn btn-lg btn-success mr-20">Update</button>
+                    <button onClick={cancelUpdate}
+                        className="btn btn lg btn-warning">Cancel</button>
+                </div>
+            </div>
+            <br />
+
+        </>
+    )
+}
+export default UpdateForm;
